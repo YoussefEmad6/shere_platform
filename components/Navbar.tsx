4664e05441fdbda1dbed12c4e8a991c3fffa21ba
Youssef Emad <youssefweb120@gmail.com>
2023-11-18 13:46:43 +0200

    last version 1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { NavLinks } from "@/constant";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
+  // console.log(session?.user);
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -21,9 +25,13 @@ const Navbar = () => {
       </div>
 
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto <Link href="/create-project">Share Work</Link>
+            <ProfileMenu session={session} />
+
+            <Link href="/create-project">
+              <Button title="Share work" />
+            </Link>
           </>
         ) : (
           <AuthProviders />
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "task.com"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "res.cloudinary.com",
+    //     pathname: "**",
+    //   },
+    // ],
+  },
 
-module.exports = nextConfig
+  experimental: {
+    serverComponentsExternalPackages: ["cloudinary", "graphql-request"],
+  },
+};
+
+module.exports = nextConfig;
